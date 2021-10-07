2f11bb07d59ff32fd36703419be1b54ab661be93
antoniocaldeira@yahoo.com <antoniocaldeira@yahoo.com>
2021-10-07 19:48:21 +0100

    Add models and services Add data

---
+++ OdetoFood.Data/Services/IRestaurantData.cs
@@ -0,0 +1,13 @@
+﻿using OdetoFood.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdetoFood.Data.Services
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+}
---
+++ OdetoFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,27 @@
+﻿using OdetoFood.Data.Models;
+using System;
+using System.Collections.Generic;
+
+namespace OdetoFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Pizza", Cuisine = CuisineType.Italian},
+                new Restaurant { Id = 2, Name = "Nona", Cuisine = CuisineType.French},
+                new Restaurant { Id = 3, Name = "Forzza", Cuisine = CuisineType.Italian},
+                new Restaurant { Id = 4, Name = "Deli", Cuisine = CuisineType.Indian}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants;
+        }
+    }
+}
